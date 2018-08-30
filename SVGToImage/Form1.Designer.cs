be02bf4dde68903d96aded5b8c1167918cae7f93
leecarry <leecarry0604@qq.com>
2018-08-30 16:36:21 +0800

    第一次可运行版提交

---
+++ SVGToImage/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace SVGToImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rtbInfo = new System.Windows.Forms.RichTextBox();
+            this.btnBegin = new System.Windows.Forms.Button();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbFormat = new System.Windows.Forms.ComboBox();
+            this.nudWidth = new System.Windows.Forms.NumericUpDown();
+            this.nudHeight = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHeight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rtbInfo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 147);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 278);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "信息窗口";
+            // 
+            // rtbInfo
+            // 
+            this.rtbInfo.Location = new System.Drawing.Point(6, 20);
+            this.rtbInfo.Name = "rtbInfo";
+            this.rtbInfo.ReadOnly = true;
+            this.rtbInfo.Size = new System.Drawing.Size(764, 252);
+            this.rtbInfo.TabIndex = 0;
+            this.rtbInfo.Text = "";
+            // 
+            // btnBegin
+            // 
+            this.btnBegin.Location = new System.Drawing.Point(633, 100);
+            this.btnBegin.Name = "btnBegin";
+            this.btnBegin.Size = new System.Drawing.Size(125, 45);
+            this.btnBegin.TabIndex = 14;
+            this.btnBegin.Text = "开始";
+            this.btnBegin.UseVisualStyleBackColor = true;
+            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(527, 86);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnOutput.TabIndex = 13;
+            this.btnOutput.Text = "选择文件夹";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(106, 86);
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(398, 21);
+            this.tbOutput.TabIndex = 12;
+            this.tbOutput.Text = "C:\\";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "输出文件夹";
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(527, 25);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(75, 23);
+            this.btnInput.TabIndex = 10;
+            this.btnInput.Text = "选择文件夹";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(106, 25);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(398, 21);
+            this.tbInput.TabIndex = 9;
+            this.tbInput.Text = "C:\\";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "SVG文件夹";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(636, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 14);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "宽";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(636, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 14);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "高";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(614, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 14);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "导出格式";
+            // 
+            // cbFormat
+            // 
+            this.cbFormat.FormattingEnabled = true;
+            this.cbFormat.Items.AddRange(new object[] {
+            "png",
+            "jpg",
+            "bmp"});
+            this.cbFormat.Location = new System.Drawing.Point(684, 69);
+            this.cbFormat.Name = "cbFormat";
+            this.cbFormat.Size = new System.Drawing.Size(93, 20);
+            this.cbFormat.TabIndex = 21;
+            this.cbFormat.Text = "png";
+            // 
+            // nudWidth
+            // 
+            this.nudWidth.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudWidth.Location = new System.Drawing.Point(684, 12);
+            this.nudWidth.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.nudWidth.Name = "nudWidth";
+            this.nudWidth.Size = new System.Drawing.Size(93, 21);
+            this.nudWidth.TabIndex = 22;
+            this.nudWidth.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // nudHeight
+            // 
+            this.nudHeight.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudHeight.Location = new System.Drawing.Point(684, 39);
+            this.nudHeight.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.nudHeight.Name = "nudHeight";
+            this.nudHeight.Size = new System.Drawing.Size(93, 21);
+            this.nudHeight.TabIndex = 23;
+            this.nudHeight.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nudHeight);
+            this.Controls.Add(this.nudWidth);
+            this.Controls.Add(this.cbFormat);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnBegin);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "批量svg转图片-https://github.com/LeeCarry/SVGToImage";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHeight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBegin;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbFormat;
+        private System.Windows.Forms.NumericUpDown nudWidth;
+        private System.Windows.Forms.NumericUpDown nudHeight;
+        private System.Windows.Forms.RichTextBox rtbInfo;
+    }
+}
+
---
+++ SVGToImage/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using Svg;
+
+namespace SVGToImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            //选择输入的文件夹
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description="请选择SVG文件的目录";
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tbInput.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            //选择输入的文件夹
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "请选择保存图片的目录";
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tbOutput.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnBegin_Click(object sender, EventArgs e)
+        {
+            //清空文本，不能给text赋值，否则颜色设置会出现问题
+            rtbInfo.Clear();
+            //检测路径合法性
+            if (!Directory.Exists(tbInput.Text)||!Directory.Exists(tbOutput.Text))
+            {
+                rtbInfo.SelectionColor = Color.Red;
+                rtbInfo.AppendText("输入路径或输出路径不存在\n");
+                return;
+
+            }
+            //准备转换工作
+            btnBegin.Enabled = false;
+            rtbInfo.SelectionColor = Color.Green;
+            rtbInfo.AppendText("开始转换,如无特殊需要请勿修改参数...\n");
+            string[] svgs = Directory.GetFiles(tbInput.Text, "*.svg");
+            rtbInfo.SelectionColor = Color.Black;
+            rtbInfo.AppendText("共读取到" + svgs.Length + "个svg文件\n");
+            int successCount = 0;
+            //开始每一次转换
+            for(int i=0;i<svgs.Length;i++)
+            {
+                Bitmap bitmap = new Bitmap((int)nudWidth.Value,(int)nudHeight.Value);
+                Graphics graphics = Graphics.FromImage(bitmap);
+                //可能会出现路径问题或其他错误，记录成功个数
+                try
+                {
+                    SvgDocument svgDocument = SvgDocument.Open(svgs[i]);
+                    ISvgRenderer renderer = SvgRenderer.FromGraphics(graphics);
+                    svgDocument.Width = (int)nudWidth.Value;
+                    svgDocument.Height = (int)nudHeight.Value;
+                    svgDocument.Draw(renderer);
+                    bitmap.Save(tbOutput.Text+"\\"+Path.GetFileNameWithoutExtension(svgs[i]) + "." + cbFormat.Text);//怕读取顺序与原顺序不一样，所以按照原名字保存
+                    successCount++;
+                    rtbInfo.AppendText("第" + (i + 1) + "个转换成功\n");
+                }
+                catch
+                {
+                    rtbInfo.SelectionColor = Color.Red;
+                    rtbInfo.AppendText("第" + (i + 1) + "个转换失败");
+                    rtbInfo.SelectionColor = Color.Black;
+                }
+            }
+            //转换完成，完成善后工作
+            rtbInfo.SelectionColor = Color.Green;
+            rtbInfo.AppendText("转换完成，共" + svgs.Length + "个文件，" + successCount + "个转换成功");
+            btnBegin.Enabled = true;
+        }
+    }
+}
